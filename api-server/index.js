8047d9e6401fd7200981a9f7df355701107642b8
Vimukthi Perera <vimper007@gmail.com>
2025-02-28 03:21:40 +0530

    add customer service and inventory service

--- api-server/index.js
+++ api-server/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { findMin, findAvg, sortArray, findValue, findMax } = require("../../util");
+const {
+  findMin,
+  findAvg,
+  sortArray,
+  findValue,
+  findMax,
+} = require("../../util");
 const app = new express();
 const port = 3000;
 const greeting = { message: "Hello Node" };
@@ -22,27 +28,35 @@ app.get("/number/avg", (req, res) => {
 
 // /number/sort?numbers=1,2,3,4,5,6.....n&type (asc|desc)
 app.get("/number/sort", (req, res) => {
-    const numbers = req.query.numbers;
-    const type = req.query.type
-    const result = sortArray(numbers.split(','),type)
-    res.json(result)
+  const numbers = req.query.numbers;
+  const type = req.query.type;
+  const result = sortArray(numbers.split(","), type);
+  res.json(result);
+});
+
+// /number/sort?numbers=1,2,3,4,5,6.....n&type (asc|desc)
+app.get("/number/sort-with-algo", (req, res) => {
+  const numbers = req.query.numbers;
+  const type = req.query.type;
+  const result = sortArray(numbers.split(","), type);
+  res.json(result);
 });
 
 // number/count?values=1,2,3,4,a,b,c,d,saman, kamal.....n&search=saman
 app.get("/number/count", (req, res) => {
-    const values = req.query.values;
-    const search = req.query.search
-    const result = findValue(values.split(','),search)
-    res.json(result)
+  const values = req.query.values;
+  const search = req.query.search;
+  const result = findValue(values.split(","), search);
+  res.json(result);
 });
 
 // /number/max?num1=1&num2=2
-app.get("/number/max",(req,res)=>{
-  const num1 = req.query.num1
-  const num2 = req.query.num2
-  const result = findMax(num1,num2);
-  res.json(result)
-})
+app.get("/number/max", (req, res) => {
+  const num1 = req.query.num1;
+  const num2 = req.query.num2;
+  const result = findMax(num1, num2);
+  res.json(result);
+});
 
 app.listen(port, () => {
   console.log(`Server is runnign on port ${port}`);
